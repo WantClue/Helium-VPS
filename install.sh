25b4ed39a1ab878ae509aec37a91e1d359923a64
WantClue <86001033+WantClue@users.noreply.github.com>
2022-01-02 02:43:55 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,6 +15,11 @@ touch /etc/openvpn/ccd/Helium-PC
 echo ifconfig-push 10.8.0.2 255.255.255.0 > /etc/openvpn/ccd/Helium-Mango
 echo ifconfig-push 10.8.0.4 255.255.255.0 > /etc/openvpn/ccd/Helium-PC
 
+echo client-config-dir ccd >> /etc/openvpn/server.conf
+echo route 192.168.8.0 255.255.255.0 >> /etc/openvpn/server.conf
+echo push "route 192.168.8.0 255.255.255.0"  >> /etc/openvpn/server.conf
+echo push "dhcp-option DNS 192.168.8.1" >> /etc/openvpn/server.conf
+
 iptables -A FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --set-mss 1240
 
 iptables -A FORWARD -i tun0 -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
