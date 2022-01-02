4b2ce7b8539a641b0e420323d4b168a8a5bc43c8
WantClue <86001033+WantClue@users.noreply.github.com>
2022-01-02 20:26:27 +0100

    Create user-install.sh

---
+++ user-install.sh
@@ -0,0 +1,10 @@
+echo client-config-dir ccd >> /etc/openvpn/server.conf
+echo route 192.168.8.0 255.255.255.0 >> /etc/openvpn/server.conf
+echo push "route 192.168.8.0 255.255.255.0"  >> /etc/openvpn/server.conf
+echo push "dhcp-option DNS 192.168.8.1" >> /etc/openvpn/server.conf
+
+touch /etc/openvpn/ccd/Helium-Mango
+touch /etc/openvpn/ccd/Helium-PC
+
+echo ifconfig-push 10.8.0.2 255.255.255.0 > /etc/openvpn/ccd/Helium-Mango
+echo ifconfig-push 10.8.0.4 255.255.255.0 > /etc/openvpn/ccd/Helium-PC
