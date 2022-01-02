d4fdbb7ef1692c9152fd3cabdf06621e6075ab32
WantClue <86001033+WantClue@users.noreply.github.com>
2022-01-02 02:12:53 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,10 +6,15 @@ wget https://raw.githubusercontent.com/Angristan/openvpn-install/master/openvpn-
 
 chmod +x debian10-vpn.sh
 
-export AUTO_INSTALL=y APPROVE_INSTALL=y APPROVE_IP=y IPV6_SUPPORT=y PORT_CHOICE=1 PROTOCOL_CHOICE=1 DNS=3 COMPRESSION_ENABLED=n CUSTOMIZE_ENC=n CLIENT=Helium-Client PASS=1
-sudo ./debian10-vpn.sh
+AUTO_INSTALL=y APPROVE_INSTALL=y APPROVE_IP=y IPV6_SUPPORT=n PORT_CHOICE=1 PROTOCOL_CHOICE=1 DNS=3 COMPRESSION_ENABLED=n CUSTOMIZE_ENC=n CLIENT=Helium-Mango PASS=1 ./openvpn-install.sh
 
+AUTO_INSTALL=y APPROVE_INSTALL=y APPROVE_IP=y IPV6_SUPPORT=n PORT_CHOICE=1 PROTOCOL_CHOICE=1 DNS=3 COMPRESSION_ENABLED=n CUSTOMIZE_ENC=n CLIENT=Helium-PC PASS=1 ./openvpn-install.sh
 
+touch /etc/openvpn/ccd/Helium-Mango
+touch /etc/openvpn/ccd/Helium-PC
+
+echo ifconfig-push 10.8.0.2 255.255.255.0 > /etc/openvpn/ccd/Helium-Mango
+echo ifconfig-push 10.8.0.4 255.255.255.0 > /etc/openvpn/ccd/Helium-PC
 
 iptables -A FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --set-mss 1240
 
@@ -20,19 +25,6 @@ iptables -A FORWARD -i eth0 -o tun0 -p tcp --syn --dport 44158 -m conntrack --ct
 iptables -A FORWARD -i eth0 -o tun0 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
 iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 44158 -j DNAT --to-destination 10.8.0.2
-
-export AUTO_INSTALL=y 
-export APPROVE_INSTALL=y
-export APPROVE_IP=y
-export IPV6_SUPPORT=y
-export PORT_CHOICE=1
-export PROTOCOL_CHOICE=1
-export DNS=3
-export COMPRESSION_ENABLED=n
-export CUSTOMIZE_ENC=n
-export CLIENT=Helium-PC
-export PASS=1
-sudo ./debian10-vpn.sh
 
 echo net.ipv4.ip_forward=1 >> /etc/sysctl.conf
 
