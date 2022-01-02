5205d668c94195b3f6a1055c73a45509cb5db324
WantClue <86001033+WantClue@users.noreply.github.com>
2022-01-02 22:06:32 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -41,6 +41,6 @@ echo client-config-dir ccd >> /etc/openvpn/server.conf
 echo "# Route to the network from host to OVPN Server" >>/etc/openvpn/server.conf
 echo route 192.168.8.0 255.255.255.0 >> /etc/openvpn/server.conf
 echo  "# Tell all clients the Home address" >>/etc/openvpn/server.conf
-echo push "route 192.168.8.0 255.255.255.0"  >> /etc/openvpn/server.conf
+echo push '"route 192.168.8.0 255.255.255.0"'  >> /etc/openvpn/server.conf
 echo "# DNS-Server for Clients (Mango)">>/etc/openvpn/server.conf
-echo push "dhcp-option DNS 192.168.8.1" >> /etc/openvpn/server.conf
+echo push '"dhcp-option DNS 192.168.8.1"' >> /etc/openvpn/server.conf
