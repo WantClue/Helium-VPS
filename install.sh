ad9a39956d79b183b3cc7760c63efc302b9f7a44
WantClue <86001033+WantClue@users.noreply.github.com>
2022-01-02 16:44:44 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ wget https://raw.githubusercontent.com/Angristan/openvpn-install/master/openvpn-
 chmod +x openvpn-install.sh
 
 AUTO_INSTALL=y APPROVE_INSTALL=y APPROVE_IP=y IPV6_SUPPORT=y PORT_CHOICE=1 PROTOCOL_CHOICE=1 DNS=3 COMPRESSION_ENABLED=n CUSTOMIZE_ENC=n CLIENT=Helium-Mango PASS=1 ENDPOINT=$(curl -4 ifconfig.co) ./openvpn-install.sh
+systemctl status openvpn
 export MENU_OPTION="1"
 export CLIENT="Helium-PC"
 export PASS="1"
@@ -24,13 +25,9 @@ echo push "route 192.168.8.0 255.255.255.0"  >> /etc/openvpn/server.conf
 echo push "dhcp-option DNS 192.168.8.1" >> /etc/openvpn/server.conf
 
 iptables -A FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --set-mss 1240
-
 iptables -A FORWARD -i tun0 -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-
 iptables -A FORWARD -i eth0 -o tun0 -p tcp --syn --dport 44158 -m conntrack --ctstate NEW -j ACCEPT
-
 iptables -A FORWARD -i eth0 -o tun0 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-
 iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 44158 -j DNAT --to-destination 10.8.0.2
 
 echo net.ipv4.ip_forward=1 >> /etc/sysctl.conf
@@ -40,5 +37,4 @@ sysctl -p
 apt-get install iptables-persistent
 
 netfilter-persistent save
-
 netfilter-persistent reload
