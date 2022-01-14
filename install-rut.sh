65492c22a3490085c74dbe745a16e4babf547326
WantClue <86001033+WantClue@users.noreply.github.com>
2022-01-14 18:22:02 +0100

    Create install-rut.sh
    This script will allow you to create a VPS Server with OpenVPN connection for your Rut240 or similar devices

---
+++ install-rut.sh
@@ -0,0 +1,46 @@
+apt update
+apt upgrade -y
+apt install iptables -y
+
+wget https://raw.githubusercontent.com/Angristan/openvpn-install/master/openvpn-install.sh
+
+chmod +x openvpn-install.sh
+
+AUTO_INSTALL=y APPROVE_INSTALL=y APPROVE_IP=y IPV6_SUPPORT=y PORT_CHOICE=1 PROTOCOL_CHOICE=1 DNS=3 COMPRESSION_ENABLED=n CUSTOMIZE_ENC=n CLIENT=Helium-Mango PASS=1 ENDPOINT=$(curl -4 ifconfig.co) ./openvpn-install.sh
+
+export MENU_OPTION="1"
+export CLIENT="Helium-PC"
+export PASS="1"
+./openvpn-install.sh
+
+iptables -A FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --set-mss 1240
+iptables -A FORWARD -i tun0 -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables -A FORWARD -i eth0 -o tun0 -p tcp --syn --dport 44158 -m conntrack --ctstate NEW -j ACCEPT
+iptables -A FORWARD -i eth0 -o tun0 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 44158 -j DNAT --to-destination 10.8.0.2
+
+echo net.ipv4.ip_forward=1 >> /etc/sysctl.conf
+
+sysctl -p
+
+apt-get install iptables-persistent -y
+
+netfilter-persistent save
+netfilter-persistent reload
+
+touch /etc/openvpn/ccd/Helium-Rut
+touch /etc/openvpn/ccd/Helium-PC
+
+echo "# static IP for the Client (Client-IP Subnet)" > /etc/openvpn/ccd/Helium-Rut
+echo ifconfig-push 10.8.0.2 255.255.255.0 >> /etc/openvpn/ccd/Helium-Rut
+echo "# static IP for the PC (PC-IP Subnet)" > /etc/openvpn/ccd/Helium-PC
+echo ifconfig-push 10.8.0.4 255.255.255.0 >> /etc/openvpn/ccd/Helium-PC
+
+echo "# Location of the config files" >> /etc/openvpn/server.conf
+echo client-config-dir ccd >> /etc/openvpn/server.conf
+echo "# Route to the network from host to OVPN Server" >>/etc/openvpn/server.conf
+echo route 192.168.1.0 255.255.255.0 >> /etc/openvpn/server.conf
+echo  "# Tell all clients the Home address" >>/etc/openvpn/server.conf
+echo push '"route 192.168.1.0 255.255.255.0"'  >> /etc/openvpn/server.conf
+echo "# DNS-Server for Clients (Rut)">>/etc/openvpn/server.conf
+echo push '"dhcp-option DNS 192.168.1.1"' >> /etc/openvpn/server.conf
